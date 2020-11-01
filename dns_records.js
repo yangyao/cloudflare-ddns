40d6f1b2a57d59df28c7ae184e94ff6fc50c6116
yangyao <yangyaohappy@outlook.com>
2020-11-01 16:37:38 +0800

    Add files via upload

---
+++ dns_records.js
@@ -0,0 +1,25 @@
+(async ()=> {
+    const request = require('request-promise');
+    const dotenv = require('dotenv');
+    dotenv.config()
+
+    const endpoint = 'https://api.cloudflare.com/client/v4';
+    // PATCH zones/:zone_identifier/dns_records/:identifier
+
+    const zoneId = process.env.ZONE_ID;
+    const authEmail =  process.env.AUTH_EMAIL;
+    const authKey = process.env.AUTH_KEY;
+
+    const params = {
+        method: 'GET',
+        url: `${endpoint}/zones/${zoneId}/dns_records`,
+        json: true,
+        headers: {
+            'X-Auth-Email' : authEmail,
+            'X-Auth-Key': authKey,
+        }
+    }
+    const response = await request(params);
+    console.log( response);
+
+})()
---
+++ index.js
@@ -0,0 +1,92 @@
+const { post } = require('request-promise');
+
+(async ()=> {
+
+    const request = require('request-promise');
+    const sleep = require('sleep-promise');
+    const dotenv = require('dotenv');
+    dotenv.config()
+
+    const endpoint = 'https://api.cloudflare.com/client/v4';
+    // PATCH zones/:zone_identifier/dns_records/:identifier
+
+    const zoneId = process.env.ZONE_ID;
+    const id = process.env.DNS_RECORD_ID;
+    const scKey = process.env.SCKEY;
+    const authEmail =  process.env.AUTH_EMAIL;
+    const authKey = process.env.AUTH_KEY;
+
+    const shell = async (cmd) => {
+        const exec = require('child_process').exec;
+        return new Promise((resolve, reject) => {
+         exec(cmd, (error, stdout, stderr) => {
+          if (error) {
+           reject(error);
+          }
+          resolve(stdout? stdout : stderr);
+         });
+        });
+    }
+
+    const retry = async (attemptTimes, duration, fn, args) => {
+        for (let i = 0; i < attemptTimes; i += 1) {
+            try {
+                const result = await fn(...args);
+                return result;
+            } catch (err) {
+                if (i >= attemptTimes - 1) {
+                    throw err;
+                }
+                logger.warn({
+                    message: 'attempt unseccessfully',
+                    detail: {attemptTimes: i + 1, err_message: err.message},
+                });
+                await sleep(duration);
+            }
+        }
+        throw new Error('attemptTimes must be greater than 0');
+    }
+
+
+    const notify = async (title, description) => {
+       const desp = description.replace(/[\n\r]/g, '\n\n');
+        const params = {
+          method: 'post',
+          url: `https://sc.ftqq.com/${scKey}.send`,
+          body: `text=${title}&desp=${desp}`,
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        }
+        await request(params);
+    }
+
+    const updateDNS = async(ip) => {
+        const params = {
+            url : `${endpoint}/zones/${zoneId}/dns_records/${id}`,
+            method: 'PATCH',
+            json: true,
+            headers: {
+                'X-Auth-Email' : authEmail,
+                'X-Auth-Key': authKey,
+            },
+            body : {
+                "type":"AAA",
+                "name":"www.v2ray-yes.cf",
+                "content": ip,
+                "ttl":120,
+                "proxied":false
+            }
+        }
+        await request(params);
+    }
+
+    try {
+        const command = "ip -6 addr | grep -v fe80 | grep 'scope global dynamic mngtmpaddr noprefixroute' | awk '{print $2}'";
+        const ip = await shell(command);
+        await retry(3, 3000, updateDNS, [ip.replace('/64', '')]);
+        await notify('DNS_UPDATE_SUCCESS', ip);
+    }catch (e) {
+        await notify('DNS_UPDATE_FAILED', e.message);
+    }
+})()
