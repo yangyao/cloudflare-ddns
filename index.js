5d88c3a966f402ed5805bf728e771535f3f95a1e
Yao Yang <y.yang@aftership.com>
2020-11-02 14:02:14 +0800

    Simplified script configuration

--- index.js
+++ index.js
@@ -1,89 +1,135 @@
-(async ()=> {
-    const request = require('request-promise');
-    const sleep = require('sleep-promise');
-    const dotenv = require('dotenv');
-    dotenv.config()
+(async () => {
+  const request = require('request-promise');
+  const sleep = require('sleep-promise');
+  const dotenv = require('dotenv');
+  dotenv.config();
 
-    const endpoint = 'https://api.cloudflare.com/client/v4';
+  const endpoint = 'https://api.cloudflare.com/client/v4';
 
-    const zoneId = process.env.ZONE_ID;
-    const id = process.env.DNS_RECORD_ID;
-    const scKey = process.env.SCKEY;
-    const authEmail =  process.env.AUTH_EMAIL;
-    const authKey = process.env.AUTH_KEY;
-    const domain = process.env.DOMAIN;
+  const shell = async (cmd) => {
+    const { exec } = require('child_process');
+    return new Promise((resolve, reject) => {
+      exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+          reject(error ? stderr : stdout);
+        }
+        resolve(stdout || stderr);
+      });
+    });
+  };
 
-    const shell = async (cmd) => {
-        const exec = require('child_process').exec;
-        return new Promise((resolve, reject) => {
-         exec(cmd, (error, stdout, stderr) => {
-          if (error) {
-           reject(error ? stderr : stdout);
-          }
-          resolve(stdout? stdout : stderr);
-         });
+  const retry = async (attemptTimes, duration, fn, args) => {
+    for (let i = 0; i < attemptTimes; i += 1) {
+      try {
+        const result = await fn(...args);
+        return result;
+      } catch (err) {
+        if (i >= attemptTimes - 1) {
+          throw err;
+        }
+        console.warn({
+          message: 'attempt unseccessfully',
+          detail: { attemptTimes: i + 1, err_message: err.message },
         });
+        await sleep(duration);
+      }
     }
+    throw new Error('attemptTimes must be greater than 0');
+  };
 
-    const retry = async (attemptTimes, duration, fn, args) => {
-        for (let i = 0; i < attemptTimes; i += 1) {
-            try {
-                const result = await fn(...args);
-                return result;
-            } catch (err) {
-                if (i >= attemptTimes - 1) {
-                    throw err;
-                }
-                console.warn({
-                    message: 'attempt unseccessfully',
-                    detail: {attemptTimes: i + 1, err_message: err.message},
-                });
-                await sleep(duration);
-            }
-        }
-        throw new Error('attemptTimes must be greater than 0');
-    }
-
-
-    const notify = async (title, description) => {
-       const desp = description.replace(/[\n\r]/g, '\n\n');
-        const params = {
-          method: 'post',
-          url: `https://sc.ftqq.com/${scKey}.send`,
-          body: `text=${title}&desp=${desp}`,
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          }
-        }
-        await request(params);
-    }
-
-    const updateDNS = async(ip) => {
-        const params = {
-            url : `${endpoint}/zones/${zoneId}/dns_records/${id}`,
-            method: 'PATCH',
-            json: true,
-            headers: {
-                'X-Auth-Email' : authEmail,
-                'X-Auth-Key': authKey,
-            },
-            body : {
-                "type":"AAAA",
-                "name":domain,
-                "content": ip,
-                "ttl":120,
-                "proxied":false
-            }
-        }
-        await request(params);
-    }
-
+  const notify = async (scKey, title, description) => {
     try {
-        const command = "ip -6 addr | grep -v fe80 | grep 'scope global dynamic mngtmpaddr noprefixroute' | awk '{print $2}'";
-        const ip = await shell(command);
-        await retry(3, 3000, updateDNS, [ip.replace('/64', '')]);
-        await notify('DNS_UPDATE_SUCCESS', ip);
-    }catch (e) {
-        await notify('DNS_UPDATE_FAILED', e.message);
+      const desp = description.replace(/[\n\r]/g, '\n\n');
+      const params = {
+        method: 'post',
+        url: `https://sc.ftqq.com/${scKey}.send`,
+        body: `text=${title}&desp=${desp}`,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      };
+      await request(params);
+    } catch (e) {
+      console.log(e.message);
     }
-})()
+  };
+
+  const getDNS = async (credentails) => {
+    const params = {
+      method: 'GET',
+      url: `${endpoint}/zones/${credentails.zoneId}/dns_records`,
+      json: true,
+      headers: {
+        'X-Auth-Email': credentails.authEmail,
+        'X-Auth-Key': credentails.authKey,
+      },
+    };
+    const response = await request(params);
+    for (const item of response.result) {
+      if (item.type === credentails.type && credentails.domain === item.name) return item;
+    }
+    return null;
+  };
+  const createDNS = async (credentails, ip) => {
+    const params = {
+      url: `${endpoint}/zones/${credentails.zoneId}/dns_records`,
+      method: 'POST',
+      json: true,
+      headers: {
+        'X-Auth-Email': credentails.authEmail,
+        'X-Auth-Key': credentails.authKey,
+      },
+      body: {
+        type: credentails.type,
+        name: credentails.domain,
+        content: ip,
+        ttl: 120,
+        proxied: false,
+      },
+    };
+    const response = await request(params);
+    return response.result;
+  };
+
+  const updateDNS = async (credentails, id, ip) => {
+    const params = {
+      url: `${endpoint}/zones/${credentails.zoneId}/dns_records/${id}`,
+      method: 'PATCH',
+      json: true,
+      headers: {
+        'X-Auth-Email': credentails.authEmail,
+        'X-Auth-Key': credentails.authKey,
+      },
+      body: {
+        type: credentails.type,
+        name: credentails.domain,
+        content: ip,
+        ttl: 120,
+        proxied: false,
+      },
+    };
+    await request(params);
+  };
+
+  const credentails = {
+    zoneId: process.env.ZONE_ID,
+    scKey: process.env.SCKEY,
+    authEmail: process.env.AUTH_EMAIL,
+    authKey: process.env.AUTH_KEY,
+    domain: process.env.DOMAIN,
+    type: process.env.DOMAIN_TYPE || 'AAAA',
+  };
+
+  try {
+    const command = "ip -6 addr | grep -v fe80 | grep 'scope global dynamic mngtmpaddr noprefixroute' | awk '{print $2}'";
+    const ip = await shell(command);
+    let record = await getDNS(credentails);
+    if (!record) {
+      record = await createDNS(credentails, ip);
+    }
+    await retry(3, 3000, updateDNS, [credentails, record.id, ip.replace('/64', '')]);
+    await notify(credentails.scKey, 'DNS_UPDATE_SUCCESS', ip);
+  } catch (e) {
+    await notify(credentails.scKey, 'DNS_UPDATE_FAILED', e.message);
+  }
+})();
